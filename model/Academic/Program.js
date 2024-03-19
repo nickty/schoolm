a5f1f547cfd676448bc4fcb9366cc6e68fd81296
Mizanur Rahman <nickty.86@gmail.com>
2024-03-19 22:05:52 +0600

    program model created

---
+++ model/Academic/Program.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const programSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+      default: '4 years',
+    },
+    code: {
+      type: String,
+      default: function () {
+        return (
+          this.name
+            .split('')
+            .map((name) => name[0])
+            .join('')
+            .toUpperCase() +
+          Math.floor(10 + Math.random() * 90) +
+          Math.floor(10 + Math.random() * 90)
+        )
+      },
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'Admin',
+      required: true,
+    },
+    teachers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Teacher',
+        default: [],
+      },
+    ],
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+        default: [],
+      },
+    ],
+    subject: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Course',
+        default: [],
+      },
+    ],
+  },
+  { timestamps: true }
+)
+
+const Program = mongoose.model('Program', programSchema)
+
+module.exports = Program
--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -20,3 +20,7 @@ const adminSchema = new mongoose.Schema(
   },
   { timestamps: true }
 )
+
+const Admin = mongoose.model('Admin', adminSchema)
+
+module.exports = Admin
