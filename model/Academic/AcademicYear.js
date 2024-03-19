320d4f5c3590e12b504ff44fda3ac7fff4000532
Mizanur Rahman <nickty.86@gmail.com>
2024-03-19 22:27:04 +0600

    academic year model created

---
+++ model/Academic/AcademicYear.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const academicYearSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    fromYear: {
+      type: Date,
+      requied: true,
+    },
+    toYear: {
+      type: Date,
+      required: true,
+    },
+    isCurrent: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'Admin',
+      required: true,
+    },
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+      },
+    ],
+    teachers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Teacher',
+      },
+    ],
+  },
+  { timestamps: true }
+)
+
+const AcademicYear = mongoose.model('AcademicYear', subjectSchema)
+
+module.exports = AcademicYear
