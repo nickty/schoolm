cc48241d80793c475a8ef5ee2ec4a68327fb4bcf
Mizanur Rahman <nickty.86@gmail.com>
2024-03-26 22:17:58 +0600

    rest of the things will come soon

--- controller/academic/examResultController.js
+++ controller/academic/examResultController.js
@@ -2,9 +2,30 @@ const AsyncHandler = require('express-async-handler')
 const Teacher = require('../../model/Staff/Teacher')
 const Exam = require('../../model/Academic/Exam')
 const ExamResult = require('../../model/Academic/ExamResults')
+const Student = require('../../model/Academic/Student')
 
 exports.checkExamResultController = AsyncHandler(async (req, res) => {
-  res.json('checking results')
+  const studentFound = await Student.findById(req.userAuth?._id)
+
+  if (!studentFound) {
+    throw new Error('No student found')
+  }
+
+  const examResult = await ExamResult.findOne({
+    studentID: studentFound?.studentId,
+    _id: req.params.id,
+  })
+  //   check if exam is published
+  if (examResult?.isPublished === false) {
+    throw new Error('Exam result is not available, check out later')
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Exam results fetched',
+    data: examResult,
+    student: studentFound,
+  })
 })
 
 exports.getAllExamResultController = AsyncHandler(async (req, res) => {
--- routes/academics/examResult.js
+++ routes/academics/examResult.js
@@ -3,12 +3,24 @@ const {
   checkExamResultController,
   getAllExamResultController,
 } = require('../../controller/academic/examResultController')
+const isStudentLoggedIn = require('../../middlewares/isStudentLoggedIn')
+const isStudent = require('../../middlewares/isStudent')
 
 const examResultRoute = express.Router()
 
 // examResultRoute.post('/', isTeacherLoggedIn, isTeacher, createExamController)
-examResultRoute.get('/:id/checking', checkExamResultController)
-examResultRoute.get('/', getAllExamResultController)
+examResultRoute.get(
+  '/:id/checking',
+  isStudentLoggedIn,
+  isStudent,
+  checkExamResultController
+)
+examResultRoute.get(
+  '/',
+  isStudentLoggedIn,
+  isStudent,
+  getAllExamResultController
+)
 // examResultRoute.put('/:id', isTeacherLoggedIn, isTeacher, updateExam)
 // examResultRoute.get('/:id', isLogin, isAdmin, getYearGroup)
 // examResultRoute.put('/:id', isLogin, isAdmin, updateYearGroup)
