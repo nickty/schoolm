a7084ee4d6b6f741c731519a7884cf7522fd55da
Mizanur Rahman <nickty.86@gmail.com>
2024-03-26 20:21:05 +0600

    checking all exam result

--- controller/academic/examResultController.js
+++ controller/academic/examResultController.js
@@ -1,7 +1,17 @@
 const AsyncHandler = require('express-async-handler')
 const Teacher = require('../../model/Staff/Teacher')
 const Exam = require('../../model/Academic/Exam')
+const ExamResult = require('../../model/Academic/ExamResults')
 
 exports.checkExamResultController = AsyncHandler(async (req, res) => {
   res.json('checking results')
+})
+
+exports.getAllExamResultController = AsyncHandler(async (req, res) => {
+  const results = await ExamResult.find()
+  res.status(200).json({
+    status: 'success',
+    message: 'Exam results fetched',
+    data: results,
+  })
 })
--- routes/academics/examResult.js
+++ routes/academics/examResult.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const {
   checkExamResultController,
+  getAllExamResultController,
 } = require('../../controller/academic/examResultController')
 
 const examResultRoute = express.Router()
 
 // examResultRoute.post('/', isTeacherLoggedIn, isTeacher, createExamController)
 examResultRoute.get('/:id/checking', checkExamResultController)
-// examResultRoute.get('/:id', getExam)
+examResultRoute.get('/', getAllExamResultController)
 // examResultRoute.put('/:id', isTeacherLoggedIn, isTeacher, updateExam)
 // examResultRoute.get('/:id', isLogin, isAdmin, getYearGroup)
 // examResultRoute.put('/:id', isLogin, isAdmin, updateYearGroup)
